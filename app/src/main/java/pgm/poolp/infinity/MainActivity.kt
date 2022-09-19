4d67016e2c0e10fdfab87aa592480e4ee2a0fd55
Nicolas <nfillion21@gmail.com>
2022-09-19 21:47:13 +0200

    initial commit

---
+++ app/src/main/java/pgm/poolp/infinity/MainActivity.kt
@@ -0,0 +1,57 @@
+package pgm.poolp.infinity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import pgm.poolp.infinity.ui.theme.InfinityTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            InfinityTheme {
+                // A surface container using the 'background' color from the theme
+                /*
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+                */
+
+
+                Scaffold {
+                    /*
+                    HomeScreen(
+                        modifier = Modifier.padding(innerPaddingModifier),
+                        viewModel = foodViewModel)
+                    */
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    InfinityTheme {
+        Greeting("Android")
+    }
+}
